d424f14805800ec521b8437a53e93ba5f2df01e9
chenyuxiang0425 <chenyuxiang0425@qq.com>
2020-07-14 00:14:39 +0800

    Modify requirements

--- lab1/test.py
+++
@@ -1,7 +0,0 @@
-for (id, name) in zip([1,2,3,4,5,6], [1,2,3,4,5,6]):
-    response = []
-    for group in [1,2,3,4,5,6]:
-        print(group)
-        response.append(group)
-        print(response)
-
